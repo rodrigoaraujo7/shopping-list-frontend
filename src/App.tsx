6273d9defa98b394c606af2fb667a11c5e2d0fc2
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-06-09 17:12:59 -0300

    feat: empty data container

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import { Card } from "./components/Card";
 import { useFolderContext } from "./context/FolderContext";
 
+import noData from "./assets/svg/no-data.svg";
+import { Button } from "./components/Button";
+
 function App() {
   const { folders } = useFolderContext();
-
-  console.log(folders);
 
   return (
     <main className="min-h-screen grid grid-cols-[1fr] md:grid-cols-[1fr_768px_1fr]">
       <div className="col-auto p-4 md:col-start-2 md:col-end-3">
-        <div className="flex justify-center items-center h-full">
-          <Card styles="outline" flex="center">
-            <h1>Teste card</h1>
-            <h1>Teste card</h1>
-            <h1>Teste card</h1>
-          </Card>
-        </div>
+        {folders.length <= 0 && (
+          <div className="flex justify-center items-center h-full">
+            <Card styles="outline" flex="center" style={{ width: "360px" }}>
+              <img src={noData} alt="" width={300} />
+
+              <div className="text-center">
+                <h1 className="text-xl font-semibold text-gray-900">
+                  Comece criando uma lista
+                </h1>
+                <h2 className="text-sm font-medium text-gray-500">
+                  Sua lista de compras inteligente será exibida aqui. Comece
+                  criando uma nova lista
+                </h2>
+              </div>
+
+              <Button>Adicionar nova lista</Button>
+            </Card>
+          </div>
+        )}
       </div>
     </main>
   );
---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import type { ComponentProps } from "react";
+
+type ButtonProps = ComponentProps<"button">;
+
+export const Button = ({ ...props }: ButtonProps) => {
+  return (
+    <button
+      className="w-full p-4 bg-primary-600 rounded-[8px] text-sm text-white font-medium"
+      {...props}
+    >
+      {props.children}
+    </button>
+  );
+};
