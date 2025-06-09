1d44d95c07ab65227cc4b6369a09f0fe812be4fc
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-06-09 16:15:10 -0300

    feat: tailwind variants and card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Card } from "./components/Card";
+
 function App() {
   return (
     <main className="min-h-screen grid grid-cols-[1fr] md:grid-cols-[1fr_768px_1fr]">
-      <div className="col-auto md:col-start-2 md:col-end-3">
-        <h1>grid</h1>
+      <div className="col-auto p-4 md:col-start-2 md:col-end-3">
+        <div className="flex justify-center items-center h-full">
+          <Card styles="outline" flex="center">
+            <h1>Teste card</h1>
+            <h1>Teste card</h1>
+            <h1>Teste card</h1>
+          </Card>
+        </div>
       </div>
     </main>
   );
---
+++ src/components/Card.tsx
@@ -0,0 +1,31 @@
+import type { ComponentProps } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+const card = tv({
+  base: "w-full h-fit p-6 flex flex-col",
+  variants: {
+    styles: {
+      soft: "bg-gray-25 rounded-xl border-[1px] border-gray-200 gap-2",
+      outline:
+        "bg-white rounded-3xl border-[1px] border-gray-200 border-dashed gap-4",
+    },
+    flex: {
+      base: "items-start justify-start",
+      center: "items-center justify-center",
+    },
+  },
+  defaultVariants: {
+    styles: "soft",
+    flex: "base",
+  },
+});
+
+type CardProps = ComponentProps<"div"> & VariantProps<typeof card>;
+
+export const Card = ({ styles, flex, className, ...props }: CardProps) => {
+  return (
+    <div className={card({ styles, flex, className })} {...props}>
+      {props.children}
+    </div>
+  );
+};
