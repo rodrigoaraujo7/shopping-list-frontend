e06918dfe7ff9e87b63529922ea85b2ea2c9cd4d
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-06-09 04:39:44 -0300

    feat: react router base

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="*" element={<h1>Página não encontrada</h1>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
